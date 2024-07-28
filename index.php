36c78d3e567fe8468a3c8034fc8a55f6fa676c06
Alaric Henrot <alaric.henrot@ospi.fr>
2024-07-28 15:51:27 +0200

    finished labour 1 need to optimise code. kept dead code for history

--- index.php
+++ index.php
@@ -2,3 +2,37 @@
 
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+
+require 'src/Fighter.php';
+
+
+$heracles = new Fighter("🧔 Heracles", 6, 20);
+
+$lion = new Fighter("🦁 Nemean lion", 13, 11);
+
+
+$round = 0;
+while ($heracles->getLife() > 0 && $lion->getLife() > 0) {
+    $round += 1;
+
+    echo "\n";
+    echo "🗡️ round " . $round . " FIGHT!!!" . "\n";
+    $heracles->fight($lion);
+    $lion->fight($heracles);
+    echo $heracles->isAlive($lion);
+    echo $lion->isAlive($heracles);
+//    echo $heracles->getName() . " : " . $heracles->getLife(). " ❤️" . "\n";
+//    echo $lion->getName() . " : " . $lion->getLife() . " ❤️" . "\n";
+//
+//    if (!$heracles->isAlive()) {
+//        echo "\n";
+//        echo "💀 " .  $heracles->getName() ." is dead" . "\n";
+//        echo "🏆 " . $lion->getName() . "won the fight (❤️ " . $lion->getLife() .")" . "\n";
+//
+//    }
+//    if(!$lion->isAlive()) {
+//        echo "\n";
+//        echo "💀 " .  $lion->getName() ." is dead!" . "\n";
+//        echo "🏆 " . $heracles->getName() . " won the fight! (remaining life: ❤️ " . $heracles->getLife() .")" . "\n";
+//    }
+}
--- src/Fighter.php
+++ src/Fighter.php
@@ -1,3 +1,62 @@
 <?php
 
 /* Fighter class definition */
+
+class Fighter
+{
+    const int MAX_LIFE = 100;
+    private int $life = self::MAX_LIFE;
+
+
+    public function __construct(
+        private readonly string $name,
+        private readonly int $dexterity,
+        private readonly int $strength
+    )
+    {
+    }
+
+    public function getLife(): int
+    {
+        return $this->life;
+    }
+
+    public function setLife(int $life): void
+    {
+        $this->life = $life;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function fight(object $target): void
+    {
+        $hit = rand(1, $this->strength);
+        $def = rand(0, $target->dexterity);
+        if ($hit - $def < 0) {
+            $damage = 0;
+        } else {
+            $damage = $hit - $def;
+        }
+
+        echo $this->getName() . " hit " . $target->getName() . " for $damage damage" . "\n";
+
+        if ($target->life -$damage < 0) {
+            $target->life = 0;
+        } else {
+            $target->life = $target->life - $damage;
+        }
+
+
+    }
+    public function isAlive($target): string
+    {
+            if ($this->getLife() === 0) {
+                return "\n" . "💀 " .  $this->getName() ." is dead" . "\n" . "🏆 " . $target->getName() . " won the fight (❤️ " . $target->getLife() .")" . "\n";
+            } else {
+                return $this->getName() . " : " . $this->getLife(). " ❤️" . "\n";
+            }
+    }
+}
